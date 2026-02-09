a16e0182ee55955e34f683c3eef875e47f5675bd
jubalaguif-hub <jubalaguif@gmail.com>
2026-02-09 12:10:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -164,8 +164,8 @@ export default defineConfig({
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
   publicDir: path.resolve(import.meta.dirname, "client", "public"),
-  build: {
-    outDir: "../dist",
+build: {
+    outDir: "../dist", // O "../" faz a pasta sair de 'client' e ir para a raiz
     emptyOutDir: true,
   },
   server: {
